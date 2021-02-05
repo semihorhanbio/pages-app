3c12a8868127e183d99728f70653293625c65f0e
sorhan <sorhan3.14@gmail.com>
2021-02-05 15:50:07 +0300

    New updates for Heroku deployment

--- pages_project/wsgi.py
+++ pages_project/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pages_project.settings')
 
 application = get_wsgi_application()
+ALLOWED_HOSTS = ['*']
